0cea8df9cb55a21dd37d3557a97ce4b9d225de13
darshan <ndarshan@gmail.com>
2016-07-21 07:20:06 +0000

    somemore setupsta

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Master to talk to mesos master 
+
+ psql -h localhost -d bar -U bar -W bar -a -f schema-pg.sql 
---
+++ schema-pg.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS registered_mesos;
+
+CREATE TABLE registered_mesos
+(
+  ip TEXT PRIMARY KEY,
+  id_name TEXT
+);
